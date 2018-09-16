d27e9052ddd61370d3c0230f116fda807096cd02
mona <php-mona@topline.com.sa>
2018-09-16 16:18:26 +0200

    new project in laravel

---
+++ app/Photos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photos extends Model
+{
+    protected $fillable = array('album_id', 'description', 'photo', 'title', 'size');
+
+    public function album(){
+        return $this->belongsTo('App\Albums');
+    }
+}
